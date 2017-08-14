1976bec9a9e34fc37b3c21a06c9607c1158c7152
Eric Pitcock <git-43p@ericpitcock.com>
2017-08-13 22:20:33 -0700

    moved stuffs

--- src/App.vue
+++ src/App.vue
@@ -1,220 +1,3 @@
-<style lang="scss">
-  @import '/assets/sass/_reset';
-  @import '/assets/sass/_variables';
-  @import '/assets/sass/_animation';
-
-  @import url('https://fonts.googleapis.com/css?family=News+Cycle|Source+Sans+Pro:400,600');
-
-  @font-face {
-    font-family: 'HouseMovements-Sign';
-    src: url('/static/fonts/HouseMovements-Sign.woff2') format('woff2');
-    font-weight: normal;
-    font-style: normal;
-  }
-
-  html {
-    height: 100%;
-  }
-
-  body, input, button {
-    font-family: 'Source Sans Pro', sans-serif;
-    font-weight: 400;
-  }
-
-  body {
-    height: 100%;
-
-    transform: translateZ(0);
-    -webkit-transform: translate3d(0, 0, 0);
-
-    background: #fff;
-    text-align: center;
-    font-size: 14px;
-    color: $black;
-    user-select: none;
-    cursor: default;
-    -webkit-font-smoothing: antialiased;
-  }
-
-  #app {
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    display: flex;
-    flex-direction: column;
-    overflow: hidden;
-    transition: background-color 0.5s ease;
-  }
-
-  .title {
-    position: absolute;
-    top: 20px;
-    left: 20px;
-    height: 20px;
-    svg {
-      vertical-align: text-top;
-      path {
-        fill: $black;
-      }
-    }
-    span {
-      padding-left: 8px;
-    }
-  }
-
-  .game-score {
-    position: absolute;
-    top: 20px;
-    right: 20px;
-    height: 20px;
-    font-weight: 600;
-    line-height: 20px;
-  }
-
-  .secret-word-container {
-    flex: 1;
-    display: flex;
-    justify-content: center;
-    position: relative;
-    z-index: 10;
-    width: 100%;
-    .secret-word {
-      display: flex;
-      align-self: flex-end;
-      &--win {
-        transform: scale(1.5);
-      }
-      &__letter {
-        position: relative;
-        align-self: flex-end;
-        font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-        font-size: 50px;
-        letter-spacing: 12px;
-        transition: all .15s cubic-bezier(0.29, 0.74, 0.04, 1.04);
-        &:first-child {
-          text-transform: uppercase;
-        }
-        &.highlight {
-          font-family: 'HouseMovements-Sign';
-          font-size: 100px;
-          letter-spacing: 0;
-          // custom kerning for T* (except l and i)
-          &:first-child[data-character-code="116"] + *:not([data-character-code="104"]):not([data-character-code="105"]) {
-            margin-left: -9px;
-          }
-        }
-      }
-    }
-    &.animated.shake {
-      -webkit-animation-duration: .5s;
-    }
-  }
-
-  .selections {
-    height: 60px;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    padding-top: 14px;
-    background: rgba(0,0,0,0.05);
-    border-top: 1px solid rgba(0,0,0,0.05);
-    display: flex;
-    justify-content: center;
-    div {
-      width: 30px;
-      height: 30px;
-      line-height: 28px;
-      text-transform: uppercase;
-      color: $white;
-      font-weight: 600;
-      background: $error-red;
-      & + div {
-        margin-left: 10px;
-      }
-    }
-  }
-
-  .definition-container {
-    height: 200px;
-    display: flex;
-    flex: 1;
-    justify-content: center;
-    &::-webkit-scrollbar {
-      display: none;
-    }
-    .definition {
-      align-self: flex-start;
-      max-width: 600px;
-      p {
-        padding-top: 30px;
-        font-size: 26px;
-        line-height: 32px;
-        font-family: 'News Cycle', sans-serif;
-        font-weight: 400;
-        &:first-letter {
-          text-transform: capitalize;
-        }
-      }
-    }
-  }
-
-  .error {
-    display: none;
-    position: absolute;
-    width: 340px;
-    top: 120px;
-    left: 50%;
-    padding: 40px 30px 30px;
-    border-radius: 3px;
-    margin-left: -170px;
-    background: $error-red;
-    color: $white;
-    font-weight: 600;
-    h1 {
-      font-size: 30px;
-      margin-bottom: 10px;
-    }
-    p {
-      margin-bottom: 30px;
-    }
-    button {
-      height: 22px;
-      display: inline-block;
-      padding: 0 10px 1px 10px;
-      border: 0;
-      background: #fff;
-      color: $dark-gray;
-      border-radius: 3px;
-      font-size: 11px;
-      font-weight: 600;
-      text-align: center;
-      text-transform: uppercase;
-      white-space: nowrap;
-      vertical-align: middle;
-      letter-spacing: 1px;
-      cursor: pointer;
-      user-select: none;
-      background-image: none;
-      box-shadow: 2px 2px 0 rgb(185, 76, 76);
-      -webkit-transition: background .2s ease-in-out;
-      transition: background .2s ease-in-out;
-      -webkit-tap-highlight-color: rgba(0,0,0,0);
-      &:focus, &:active {
-        outline: 0;
-      }
-      &[disabled] {
-        opacity: 0.5;
-        cursor: default;
-        &:hover {
-          background: transparent;
-        }
-      }
-    }
-  }
-</style>
-
 <template>
   <div id="app" :style="{ background: 'hsla(140, 40%, ' + backgroundLightness + '%, 1)' }">
     <div class="title">
@@ -488,3 +271,220 @@
     }
   }
 </script>
+
+<style lang="scss">
+  @import '/assets/sass/_reset';
+  @import '/assets/sass/_variables';
+  @import '/assets/sass/_animation';
+
+  @import url('https://fonts.googleapis.com/css?family=News+Cycle|Source+Sans+Pro:400,600');
+
+  @font-face {
+    font-family: 'HouseMovements-Sign';
+    src: url('/static/fonts/HouseMovements-Sign.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  html {
+    height: 100%;
+  }
+
+  body, input, button {
+    font-family: 'Source Sans Pro', sans-serif;
+    font-weight: 400;
+  }
+
+  body {
+    height: 100%;
+
+    transform: translateZ(0);
+    -webkit-transform: translate3d(0, 0, 0);
+
+    background: #fff;
+    text-align: center;
+    font-size: 14px;
+    color: $black;
+    user-select: none;
+    cursor: default;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  #app {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    transition: background-color 0.5s ease;
+  }
+
+  .title {
+    position: absolute;
+    top: 20px;
+    left: 20px;
+    height: 20px;
+    svg {
+      vertical-align: text-top;
+      path {
+        fill: $black;
+      }
+    }
+    span {
+      padding-left: 8px;
+    }
+  }
+
+  .game-score {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    height: 20px;
+    font-weight: 600;
+    line-height: 20px;
+  }
+
+  .secret-word-container {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    position: relative;
+    z-index: 10;
+    width: 100%;
+    .secret-word {
+      display: flex;
+      align-self: flex-end;
+      &--win {
+        transform: scale(1.5);
+      }
+      &__letter {
+        position: relative;
+        align-self: flex-end;
+        font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+        font-size: 50px;
+        letter-spacing: 12px;
+        transition: all .15s cubic-bezier(0.29, 0.74, 0.04, 1.04);
+        &:first-child {
+          text-transform: uppercase;
+        }
+        &.highlight {
+          font-family: 'HouseMovements-Sign';
+          font-size: 100px;
+          letter-spacing: 0;
+          // custom kerning for T* (except l and i)
+          &:first-child[data-character-code="116"] + *:not([data-character-code="104"]):not([data-character-code="105"]) {
+            margin-left: -9px;
+          }
+        }
+      }
+    }
+    &.animated.shake {
+      -webkit-animation-duration: .5s;
+    }
+  }
+
+  .selections {
+    height: 60px;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    padding-top: 14px;
+    background: rgba(0,0,0,0.05);
+    border-top: 1px solid rgba(0,0,0,0.05);
+    display: flex;
+    justify-content: center;
+    div {
+      width: 30px;
+      height: 30px;
+      line-height: 28px;
+      text-transform: uppercase;
+      color: $white;
+      font-weight: 600;
+      background: $error-red;
+      & + div {
+        margin-left: 10px;
+      }
+    }
+  }
+
+  .definition-container {
+    height: 200px;
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    &::-webkit-scrollbar {
+      display: none;
+    }
+    .definition {
+      align-self: flex-start;
+      max-width: 600px;
+      p {
+        padding-top: 30px;
+        font-size: 26px;
+        line-height: 32px;
+        font-family: 'News Cycle', sans-serif;
+        font-weight: 400;
+        &:first-letter {
+          text-transform: capitalize;
+        }
+      }
+    }
+  }
+
+  .error {
+    display: none;
+    position: absolute;
+    width: 340px;
+    top: 120px;
+    left: 50%;
+    padding: 40px 30px 30px;
+    border-radius: 3px;
+    margin-left: -170px;
+    background: $error-red;
+    color: $white;
+    font-weight: 600;
+    h1 {
+      font-size: 30px;
+      margin-bottom: 10px;
+    }
+    p {
+      margin-bottom: 30px;
+    }
+    button {
+      height: 22px;
+      display: inline-block;
+      padding: 0 10px 1px 10px;
+      border: 0;
+      background: #fff;
+      color: $dark-gray;
+      border-radius: 3px;
+      font-size: 11px;
+      font-weight: 600;
+      text-align: center;
+      text-transform: uppercase;
+      white-space: nowrap;
+      vertical-align: middle;
+      letter-spacing: 1px;
+      cursor: pointer;
+      user-select: none;
+      background-image: none;
+      box-shadow: 2px 2px 0 rgb(185, 76, 76);
+      -webkit-transition: background .2s ease-in-out;
+      transition: background .2s ease-in-out;
+      -webkit-tap-highlight-color: rgba(0,0,0,0);
+      &:focus, &:active {
+        outline: 0;
+      }
+      &[disabled] {
+        opacity: 0.5;
+        cursor: default;
+        &:hover {
+          background: transparent;
+        }
+      }
+    }
+  }
+</style>
